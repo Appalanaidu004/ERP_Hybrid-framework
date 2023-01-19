36f2342d48f4c75eb4d4ac9b3ca6f6d5ba4abe17
Appalanaidu0315 <118420933+Appalanaidu0315@users.noreply.github.com>
2023-01-19 14:54:58 +0530

    Add files via upload

---
+++ Loginpage.java
@@ -0,0 +1,69 @@
+package orangehrmLibraries;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Apputils.Apputils;
+
+public class Loginpage extends Apputils
+{
+	
+
+	public void login(String uid,String pwd)
+	{		
+		driver.findElement(By.id("txtUsername")).sendKeys(uid);
+		driver.findElement(By.id("txtPassword")).sendKeys(pwd);
+		driver.findElement(By.id("btnLogin")).click();		
+	}
+	
+	public boolean isAdminModuleDisplayed()
+	{
+		
+		if(driver.findElement(By.linkText("Admin")).isDisplayed())
+		{
+			return true;
+		}else
+		{         
+			return false;
+		}
+	}
+	
+	public void logout()
+	{
+		driver.findElement(By.partialLinkText("Welcome")).click();
+		driver.findElement(By.linkText("Logout")).click();
+	}
+	
+	public boolean isErrMsgDisplayed()
+	{
+		String errmsg = driver.findElement(By.id("spanMessage")).getText();
+		if(errmsg.toLowerCase().contains("invalid"))
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+	}
+	
+	
+	public boolean isEmpModuleDisplayed()
+	{
+		
+		try 
+		{
+			driver.findElement(By.linkText("Admin")).isDisplayed();
+			return false;
+			
+		} catch (Exception e) 
+		{
+			return true;
+		}		
+	}
+
+	
+	
+	
+
+}
